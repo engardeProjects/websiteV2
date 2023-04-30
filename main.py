197a49445381f729856d7c05da40ebfe1e5f4e06
engardeProjects <engarde@brandeis.edu>
2023-04-30 13:30:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="test",page_icon=":tada:",layout="wide")
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.subheader('test')
+
