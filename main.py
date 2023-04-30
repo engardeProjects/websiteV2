b7a74ed105d7172a6bc175be1a03b7819cbb6958
engardeProjects <engarde@brandeis.edu>
2023-04-30 13:37:31 -0400

    test fix
    attempt to remove footer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="test",page_icon=":tada:",layout="wide")
+st.set_page_config(page_title="test2",page_icon=":tada:",layout="wide")
 hide_decoration_bar_style = '''
     <style>
         header {visibility: hidden;}
@@ -14,6 +14,13 @@ footer {visibility: hidden;}
 header {visibility: hidden;}
 
 """
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 hide_streamlit_style = """
